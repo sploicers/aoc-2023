f75a91d9c7f7bf0448b891ed79ce0faa92cf2734
sploicers <7642631+sploicers@users.noreply.github.com>
2023-12-11 23:52:24 +1300

    Continue attempt

--- src/day3/part1.rs
+++ src/day3/part1.rs
@@ -1,73 +1,102 @@
 use std::collections::{HashMap, HashSet};
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 enum Tile {
-	Symbol, Part(MachinePart)
-}
-
-#[derive(Debug)]
-struct MachinePart {
-	part_num: u32,
+    Symbol,
+    Part(u32),
 }
 
 #[derive(Debug)]
 struct Grid {
-	coordinates: HashMap<(usize, usize), Tile>
+    coordinates: HashMap<(usize, usize), Tile>,
 }
 
 impl Grid {
-	pub fn from_lines(lines: impl Iterator<Item = String>) -> Self {
-		let mut coordinates: HashMap<(usize, usize), Tile> = HashMap::new();
+    pub fn from_lines(lines: impl Iterator<Item = String>) -> Self {
+        let mut coordinates: HashMap<(usize, usize), Tile> = HashMap::new();
 
-		for (y, line) in lines.enumerate() {
-			let mut x = 0;
-			let mut last_num_start_pos: Option<usize> = None;
+        for (y, line) in lines.enumerate() {
+            let mut x = 0;
+            let mut last_num_start_pos: Option<usize> = None;
 
-			loop {
-				let current_slice = &mut line[x..].chars();
+            loop {
+                let current_slice = &mut line[x..].chars();
 
-				if let Some(current_char) = current_slice.next() {
-					if current_char.is_numeric() {
-						if last_num_start_pos.is_none() {
-							last_num_start_pos = Some(x);
-						}
-					} else {
-						if current_char == '.' {
-							coordinates.insert((x, y), Tile::Symbol);
-						}
-						if let Some(num_start) = last_num_start_pos {
-							let part_number: u32 = String::from_iter(line[num_start..x].chars())
-								.parse()
-								.expect("Fatal - failed to parse supposed part number");
+                if let Some(current_char) = current_slice.next() {
+                    if current_char.is_numeric() {
+                        if last_num_start_pos.is_none() {
+                            last_num_start_pos = Some(x);
+                        }
+                    } else {
+                        if current_char != '.' {
+                            coordinates.insert((x, y), Tile::Symbol);
+                        }
+                        if let Some(num_start) = last_num_start_pos {
+                            let part_number: u32 = String::from_iter(line[num_start..x].chars())
+                                .parse()
+                                .expect("Fatal - failed to parse supposed part number");
 
-							for num_x in num_start..=x {
-								coordinates.insert((num_x, y), Tile::Part(MachinePart { part_num: part_number }));
-							}
-							last_num_start_pos = None;
-						}
-					}
-					x += 1;
-				} else {
-					break;
-				}
-			}
-		}
+                            for num_x in num_start..x {
+                                coordinates.insert((num_x, y), Tile::Part(part_number));
+                            }
+                            last_num_start_pos = None;
+                        }
+                    }
+                    x += 1;
+                } else {
+                    break;
+                }
+            }
+        }
 
-		Self { coordinates }
-	}
+        Self { coordinates }
+    }
 
-	pub fn get(&self, x: usize, y: usize) -> Option<&Tile> {
-		self.coordinates.get(&(x, y))
-	}
+    pub fn get(&self, x: usize, y: usize) -> Option<&Tile> {
+        self.coordinates.get(&(x, y))
+    }
 
-	pub fn neighbours(&self, x: usize, y: usize) -> Vec<Tile> {
-		todo!()
-	}
+    pub fn neighbours(&self, x: usize, y: usize) -> impl Iterator<Item = &Tile> {
+        let mut neighbours: Vec<Option<&Tile>> = vec![];
+        for y0 in -1..=1 {
+            for x0 in -1..=1 {
+                if !(x == 0 && y == 0) {
+                    neighbours.push(self.get((x as i32 + x0) as usize, (y as i32 + y0) as usize))
+                }
+            }
+        }
+        neighbours.into_iter().flatten()
+    }
+
+    pub fn part_number_total(&self) -> u32 {
+        let mut total = 0;
+        let mut parts_uniq: HashSet<&Tile> = HashSet::new();
+
+        for ((x, y), tile) in self.coordinates.iter() {
+            match tile {
+                Tile::Symbol => {
+                    for neighbour in self.neighbours(*x, *y) {
+                        match neighbour {
+                            Tile::Part(_) => {
+                                parts_uniq.insert(neighbour);
+                            }
+                            _ => (),
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+        for t in parts_uniq {
+            match t {
+                Tile::Part(part_num) => total += part_num,
+                _ => (),
+            }
+        }
+        total
+    }
 }
 
-
 pub fn solution(lines: impl Iterator<Item = String>) -> u32 {
-	let grid = Grid::from_lines(lines);
-	println!("{:?}", grid);
-	0
+    Grid::from_lines(lines).part_number_total()
 }
